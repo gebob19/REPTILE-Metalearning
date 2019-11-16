e3778662ca187857f47e7c207fcb4745a55e8053
Brennan Gebotys <gebob19.git@gmail.com>
2019-11-16 07:54:58 -0500

    remove

--- fetch_data.sh
+++ fetch_data.sh
@@ -42,14 +42,6 @@ if [ ! -d data/omniglot ]; then
     unzip $filename
     rm $filename
 
-    # download training split run from source code
-    export fileid=1bbyprI8HiUCZorpN0NN5r7nUh-hIBa6I
-    export filename=train.zip
-
-    curl -L -o $filename 'https://docs.google.com/uc?export=download&id='$fileid
-    unzip $filename
-    rm $filename
-
     mv vinyals data/omniglot/splits/
 fi
 
